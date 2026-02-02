edbaabfc2b6abfdea35ddf37ec6654645219b792
Dima <trovine12.95@gmail.com>
2026-02-02 13:57:17 -0500

    fix

--- recipe_project/recipes/views.py
+++ recipe_project/recipes/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Recipe
-from .forms import RecipeForm # You need to create this standard ModelForm
-from recipe_scrapers import scrape_me
+from django.views.generic import TemplateView
 from django.contrib import messages
+from recipe_scrapers import scrape_me
 
+from .models import Recipe
+from .forms import RecipeForm
+
+# --- 1. LANDING PAGE VIEW ---
+# This serves the root (/) path for non-logged-in users
+class LandingPageView(TemplateView):
+    template_name = "landing.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        # If user is already logged in, send them to their recipes
+        if request.user.is_authenticated:
+            return redirect('recipe_list')
+        return super().dispatch(request, *args, **kwargs)
+
+# --- 2. RECIPE LIST VIEW ---
+# Required for the redirect in create_recipe and login_redirect
+@login_required
+def recipe_list(request):
+    recipes = Recipe.objects.filter(owner=request.user)
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+# --- 3. CREATE RECIPE VIEW ---
 @login_required
 def create_recipe(request):
     initial_data = {}
@@ -22,7 +43,7 @@ def create_recipe(request):
             }
             messages.success(request, "Recipe imported! Add an image or save.")
         except Exception as e:
-            messages.error(request, "Could not scrape that URL.")
+            messages.error(request, f"Could not scrape that URL: {e}")
 
     # 2. Form Handling
     if request.method == 'POST':
@@ -37,7 +58,7 @@ def create_recipe(request):
                 messages.warning(request, "Image removed. Upgrade to Premium to save photos.")
             
             recipe.save()
-            return redirect('recipe_list') # Assume you have a list view
+            return redirect('recipe_list')
     else:
         form = RecipeForm(initial=initial_data)
 
