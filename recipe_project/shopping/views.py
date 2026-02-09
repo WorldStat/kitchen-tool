c3160dc8f9331406a784ed1d926c7c0d14b899b7
Dima <trovine12.95@gmail.com>
2026-02-09 14:48:29 -0500

    fix

--- recipe_project/shopping/views.py
+++ recipe_project/shopping/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
